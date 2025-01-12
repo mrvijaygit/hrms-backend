242d60208c38950fd360cc4d5c28b95603f8e2c7
vijaymr <101705089+mrvijaygit@users.noreply.github.com>
2025-01-12 13:07:24 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-    host:"147.93.96.119",
+    host:"localhost",
     user:"u343166541_HRMDB",
     password:"Zap@hrms_99",
     database:"u343166541_HRMDB"
